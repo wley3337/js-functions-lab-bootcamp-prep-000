e50c91a0568ec165e3e42cf87264030cddf9181e
wley3337@gmail.com <wley3337@gmail.com>
2018-02-17 22:51:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(holiday, day){
+function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`;
 }
